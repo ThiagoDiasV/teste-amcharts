902161fb402190719512fd74232172610f149bfb
ThiagoDiasV <thiago76ers@gmail.com>
2020-05-08 13:20:29 -0300

    test

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
-var chart = am4core.create('chartdiv', am4maps.MapChart);
+am4core.ready(function () {
+  var chart = am4core.create('chartdiv', am4maps.MapChart);
 
-chart.geodata = am4geodata_brCE;
+  // chart.geodata = am4geodata_brCE;
 
-// também não funciona
-// chart.geodataSource.url = 'brCE.json';
+  // também não funciona
+  chart.geodataSource.url = 'brCE.json';
 
-chart.projection = new am4maps.projections.Mercator();
+  chart.projection = new am4maps.projections.Mercator();
 
-var polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
+  var polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
 
-polygonSeries.useGeodata = true;
+  polygonSeries.useGeodata = true;
 
-var polygonTemplate = polygonSeries.mapPolygons.template;
-polygonTemplate.tooltipText = '{NM_MUNICIP}';
-polygonTemplate.fill = am4core.color('#74B266');
+  var polygonTemplate = polygonSeries.mapPolygons.template;
+  polygonTemplate.tooltipText = '{NM_MUNICIP}';
+  polygonTemplate.fill = am4core.color('#74B266');
 
-var hs = polygonTemplate.states.create('hover');
-hs.properties.fill = am4core.color('#367B25');
+  var hs = polygonTemplate.states.create('hover');
+  hs.properties.fill = am4core.color('#367B25');
 
-chart.zoomControl = new am4maps.ZoomControl();
+  chart.zoomControl = new am4maps.ZoomControl();
+});
